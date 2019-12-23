69f16b7bc6b8fc9985edc68b70a81f47e176bafc
saucedbenny <management@circlebeats.com>
2019-12-23 16:54:50 -0500

    101 state management

--- v1.0.0/services/get/index.js
+++ v1.0.0/services/get/index.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 
 module.exports = async function (fastify) {
   fastify.get('/beats/:name', async function (request,reply) {
+    //101 State Change
     const pathLink = path.join(__dirname,'..','..','storage','mp3',`${request.params.name}.mp3`)
     fs.readFile(pathLink, function(err,fileBuffer) {
       //console.log(fileBuffer)
